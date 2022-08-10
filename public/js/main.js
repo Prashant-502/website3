c5a7f714ca81a5e1e8d5507e92058c41c579b31b
prashant.shigvan <prashant.shigvan@teknopoint.in>
2022-08-10 17:01:52 +0530

    second commit

--- public/js/main.js
+++ public/js/main.js
@@ -102,3 +102,9 @@
     
 })(jQuery);
 
+jQuery.event.special.touchstart = {
+    setup: function( _, ns, handle ) {
+        this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+    }
+  };
+
